3ef568a75c71c2465c7c52833f54e94fef94811f
Matthew Boehlig <thetamind@gmail.com>
2017-03-15 02:02:57 -0500

    More tests

--- apps/poker/test/deck_test.exs
+++ apps/poker/test/deck_test.exs
@@ -7,5 +7,17 @@ defmodule Poker.DeckTest do
     test "deck has 52 cards" do
       assert Enum.count(Deck.new()) == 52
     end
+
+    test "is without duplicates" do
+      assert Enum.count(Enum.uniq(Deck.new())) == 52
+    end
+
+    test "has 13 cards of each suite" do
+      counts = Deck.new()
+      |> Enum.reduce %{}, fn({_rank, suit}, acc) ->
+        Map.update(acc, suit, 1, &(&1 + 1))
+      end
+      assert counts == %{spades: 13, clubs: 13, hearts: 13, diamonds: 13}
+    end
   end
 end
