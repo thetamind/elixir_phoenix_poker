7ec70dbf744f9cf5bf68f429029866ff17c34107
Matthew Boehlig <thetamind@gmail.com>
2017-03-15 16:02:43 -0500

    Try ExUnit setup block
    Doc: https://hexdocs.pm/ex_unit/ExUnit.Callbacks.html

--- apps/poker/test/deck_test.exs
+++ apps/poker/test/deck_test.exs
@@ -4,19 +4,21 @@ defmodule Poker.DeckTest do
   alias Poker.Deck
 
   describe "Deck.new/0" do
-    test "deck has 52 cards" do
-      assert Enum.count(Deck.new()) == 52
+    setup do: [deck: Deck.new()]
+
+    test "deck has 52 cards", %{deck: deck} do
+      assert Enum.count(deck) == 52
     end
 
-    test "is without duplicates" do
-      assert Enum.count(Enum.uniq(Deck.new())) == 52
+    test "is without duplicates", %{deck: deck} do
+      assert Enum.count(Enum.uniq(deck)) == 52
     end
 
-    test "has 13 cards of each suite" do
-      counts = Deck.new()
-      |> Enum.reduce %{}, fn({_rank, suit}, acc) ->
+    test "has 13 cards of each suite", %{deck: deck} do
+      counts = deck
+      |> Enum.reduce(%{}, fn({_rank, suit}, acc) ->
         Map.update(acc, suit, 1, &(&1 + 1))
-      end
+      end)
       assert counts == %{spades: 13, clubs: 13, hearts: 13, diamonds: 13}
     end
   end
