891b8ae8a450e4c8f811684574b94b22907dcbd2
Matthew Boehlig <thetamind@gmail.com>
2017-03-18 02:21:16 -0500

    Generate deck grouped by suit, not rank

--- apps/poker/lib/poker/deck.ex
+++ apps/poker/lib/poker/deck.ex
@@ -1,9 +1,13 @@
 defmodule Poker.Deck do
+  @moduledoc """
+  A standard 52 card deck.
+  """
+
   @ranks Enum.to_list(2..14)
   @suits [:spades, :clubs, :hearts, :diamonds]
 
   def new do
-    for rank <- @ranks, suit <- @suits do
+    for suit <- @suits, rank <- @ranks do
       {rank, suit}
     end |> Enum.shuffle()
   end
